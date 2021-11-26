49c8b84ec8d62fa29aee8a47d3da35a72204b286
krishpranav <krisna.pranav@gmail.com>
2021-11-26 18:40:46 +0530

    sockets: client
    client functions

--- sockets/client.go
+++ sockets/client.go
@@ -1,1 +1,42 @@
 package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	ID   string
+	Conn *websocket.Conn
+	Pool *Pool
+	mu   sync.Mutex
+}
+
+type Message struct {
+	Type int    `json:"type"`
+	Body string `json:"body"`
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregsiter <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		message := Message{Type: messageType, Body: string(p)}
+		// TODO: broadcast funcs need to add
+		c.Pool.Broadcast <- message
+		fmt.Printf("Message Received >> %+v\n", message)
+
+	}
+}
