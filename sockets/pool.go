0a34a767c853258a35046cd792a26ae54dc50d24
krishpranav <krisna.pranav@gmail.com>
2021-11-26 18:46:17 +0530

    sockets: pool
    connections added

--- sockets/pool.go
+++ sockets/pool.go
@@ -2,6 +2,53 @@
 
 package websocket
 
+import "fmt"
+
 type Pool struct {
-	Register *Client
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[*Client]bool
+	Broadcast  chan Message
+}
+
+func NewPool() *Pool {
+	return &Pool{
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[*Client]bool),
+		Broadcast:  make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			/* log message */
+			fmt.Println("Size of connection pool: ", len(pool.Clients))
+			for client, _ := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New User Has Joined...!!!"})
+			}
+			break
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			/* log message */
+			fmt.Println("Size of connection pool: ", len(pool.Clients))
+			for client, _ := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User disconnected...."})
+			}
+			break
+		case message := <-pool.Broadcast:
+			/* log msg */
+			fmt.Println("Sending message to all clients in pool")
+			for client, _ := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					return
+				}
+			}
+		}
+	}
 }
